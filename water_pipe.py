3ba39a4891f5f98896240d63d4af9fee1f67580b
Su Guangjun <guangjun@Sus-MacBook-Pro.local>
2017-01-26 17:20:32 -0800

    Update Python Code

--- water_pipe.py
+++ water_pipe.py
@@ -1,6 +1,5 @@
 import numpy as np
 # Change the file name
-# 
 f = open("by2.txt", "r")
 data = []
 g = 9.8
@@ -61,8 +60,10 @@ for num, i in enumerate(end):
 	end_demand.append(float(data[i + 3][7])/1000.0)
 	d[num_v] -= float(data[i + 3][7])/1000.0
 
-
-#----------Stage 1: Use Maximum Flow Minimum Cost to caculate an initial solution----------#
+"""
+Stage 1:
+Use Maximum Flow Minimum Cost method to caculate an initial solution.
+"""
 from cvxpy import *
 A = np.array(A)
 d = np.array(d)
@@ -70,42 +71,51 @@ L = np.array(L)
 
 q = Variable(num_e + num_end)
 objective = Minimize(1/3.0 * sum(L * q**3))
-constraints = [0 <= q, A*q <= d, q[8] >= 0.01, q[9] >= 0.01, q[10] >= 0.01]
+constraints = [0 <= q, A*q <= d]
 prob = Problem(objective, constraints)
 result = prob.solve(solver = "CVXOPT")
 
 q_value = q.value
+h_value = np.linalg.lstsq(A.transpose(), np.diag(L)*np.square(q_value))[0]
 
-
-#---------Stage 2:    
-for i in range(20):
+"""
+Stage 2:
+Within this stage, we utilize an iterative procedure to find the approximate optimal solution
+Sub-stage 2.1:
+	Slightly adjust head pressure
+Sub-stage 2.2:
+	Slightly adjust flow
+"""
+  
+for i in range(100):
 	print "-----------------------Iteration %d-----------------------" %i
 	# First Problem: fixed q_value, and add head constraint as variables
-	q = Variable(num_e + num_end)
 	hc = Variable(num_v + 1)
-	penalty = sum(abs(A.transpose()*hc - np.diag(L)*np.square(q_value)))
-	objective = Minimize(1/3.0 * sum(L * q**3) + penalty)
-	constraints = [0 <= q, A*q <= d, q[8] >= 0.01, q[9] >= 0.01, q[10] >= 0.01, 0 <= hc, hc[9] == 0, hc[8] >= h[8], hc[4] >= h[4], hc[5] >= h[5]]
+	objective = Minimize(10000*norm2(A.transpose()*hc - np.diag(L)*np.square(q_value)) + norm2(hc - h_value))
+	constraints = [0 <= hc, hc[9] == 0, hc[8] >= h[8], hc[4] >= h[4], hc[5] >= h[5]]
 	prob = Problem(objective, constraints)
 	result = prob.solve(solver = "CVXOPT")
 
 	h_value = hc.value
-	q_value = q.value
-	print "Objective value is %.4f; Error values Ah-Lq^2: %.4f" % (objective.value - penalty.value, sum(abs(A.transpose()*h_value - np.diag(L)*np.square(q_value))).value)
+	# print "Objective value is %.4f; Error values Ah-Lq^2: %.4f" % (objective.value - penalty.value, sum(abs(A.transpose()*h_value - np.diag(L)*np.square(q_value))).value)
+	
 	# Second Problem: fixed h_value, and only optimize the q
 	q = Variable(num_e + num_end)
 	tmp = A.transpose()*h_value
 	tmp.A1[tmp.A1 < 0] = 0
 	anchor = np.sqrt(tmp.A1/L)
 	penalty = sum(abs(q - anchor))
-	objective = Minimize(1/3.0 * sum(L * q**3) + 1000*penalty)
+	objective = Minimize(1/3.0 * sum(L * q**3) + 1000*norm2(q - anchor) + 100*norm2(q - q_value))
 	constraints = [0 <= q, A*q <= d, q[8] >= 0.01, q[9] >= 0.01, q[10] >= 0.01]
 	prob = Problem(objective, constraints)
 	result = prob.solve(solver = "CVXOPT")
 
 	q_value = q.value
 
-	print "Objective value is %.4f; Error values Ah-Lq^2: %.4f" % (objective.value - penalty.value, sum(abs(A.transpose()*h_value - np.diag(L)*np.square(q_value))).value)
+	obj_adj = 1/3.0 * sum(L * q**3).value
+	print "Objective value is %.4f; Error values Ah-Lq^2: %.4f" % (obj_adj, sum(abs(A.transpose()*h_value - np.diag(L)*np.square(q_value))).value)
 	print "Head value is as follow:"
 	print h_value
 
+
+# hhh = np.linalg.lstsq(A.transpose(), np.diag(L)*np.square(q_value))[0]
